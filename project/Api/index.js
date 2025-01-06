20a7178f86263b5899ca212f5d5e7dbc0c8296ca
Emiliano Rozas <emilianorozas@gmail.com>
2025-01-06 13:39:37 +0100

    Config vercel

--- project/Api/index.js
+++ project/Api/index.js
@@ -1,38 +1,43 @@
-import 'dotenv/config';
-import db from 'dat';
-import express from 'express';
-import cors from 'cors';
+import 'dotenv/config'
+import db from 'dat'
+import express from 'express'
+import cors from 'cors'
 
-import { productsRouter, usersRouter, cartRouter, orderRouter, paymentsRouter } from './routes/index.js';
-import { errorHandler } from './middleware/index.js';
+import { productsRouter, usersRouter, cartRouter, orderRouter, paymentsRouter } from './routes/index.js'
+import { errorHandler } from './middleware/index.js'
 
-const server = express();
+const server = express()
 
 // Middleware y configuraciones
-server.use(cors());
+server.use(
+    cors({
+        origin: ['http://localhost:7000', 'https://entelequia-frontend.vercel.app/', 'http://localhost:5173'],
+        credentials: true,
+    })
+)
 
-server.get('/', (_, res) => res.send('Welcome to Entelequia Comic - Book Store API!'));
-server.use('/users', usersRouter);
-server.use('/products', productsRouter);
-server.use('/carts', cartRouter);
-server.use('/orders', orderRouter);
-server.use('/payments', paymentsRouter);
+server.get('/', (_, res) => res.send('Welcome to Entelequia Comic - Book Store API!'))
+server.use('/users', usersRouter)
+server.use('/products', productsRouter)
+server.use('/carts', cartRouter)
+server.use('/orders', orderRouter)
+server.use('/payments', paymentsRouter)
 
-server.use(errorHandler);
+server.use(errorHandler)
 
 // Conexión a la base de datos
 db.connect(process.env.MONGO_URL)
     .then(() => console.log('Connected to db'))
-    .catch((err) => {
-        console.error('Database connection failed', err);
-        process.exit(1);
-    });
+    .catch((error) => {
+        console.error('Database connection failed', error)
+        process.exit(1)
+    })
 
 // Modo de ejecución local (cuando no está en Vercel)
 if (!process.env.VERCEL) {
-    const PORT = process.env.PORT || 7000;
-    server.listen(PORT, () => console.log(`API listening on port ${PORT}`));
+    const PORT = process.env.PORT || 7000
+    server.listen(PORT, () => console.log(`API listening on port ${PORT}`))
 }
 
 // Exporta el servidor para Vercel
-export default server;
+export default server
